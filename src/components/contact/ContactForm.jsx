7268be5eba4bd21dc867dc6c7ce44e69e2615a92
Piyush Kumar <piyushkumar1116@gmail.com>
2025-02-23 00:52:05 +0530

    refactor ContactForm component to improve form submission handling and add timestamps for submitted data

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -13,136 +13,145 @@ import emailjs from "@emailjs/browser";
 const env = import.meta.env;
 
 function ContactForm() {
-    const [formLoad, setfromLoad] = useState(false);
-    const [showSubmitAnimation, setShowSubmitAnimation] = useState(false);
+  const [formLoad, setfromLoad] = useState(false);
+  const [showSubmitAnimation, setShowSubmitAnimation] = useState(false);
 
-    const form = useForm({
-        initialValues: {
-            name: "",
-            email: "",
-            mobile: "",
-            message: "",
-        },
+  const form = useForm({
+    initialValues: {
+      name: "",
+      email: "",
+      mobile: "",
+      message: "",
+    },
 
-        validate: {
-            name: (value) => (value.length < 3 ? "Please enter a valid name" : null),
-            email: (value) =>
-                /^\S+@\S+$/.test(value) ? null : "Please enter valid email",
-            mobile: (value) =>
-                value.length < 10 ? "Please enter valid number" : null,
-        },
-    });
+    validate: {
+      name: (value) => (value.length < 3 ? "Please enter a valid name" : null),
+      email: (value) =>
+        /^\S+@\S+$/.test(value) ? null : "Please enter valid email",
+      mobile: (value) =>
+        value.length < 10 ? "Please enter valid number" : null,
+    },
+  });
 
-    //db collection
-    const userCollectionRef = collection(db, "contacted-user");
+  //db collection
+  const userCollectionRef = collection(db, "contacted-user");
 
-    //  Send email using EmailJS in the browser
-    const handleEmailjs = async (values) => {
-        const emailParams = {
-            from_name: values.name,
-            from_email: values.email,
-            from_mobile: values.mobile,
-            message: values.message,
-        };
-
-        await emailjs.send(
-            env.VITE_emailjs_serviceId,
-            env.VITE_emailjs_template_Id,
-            emailParams,
-            env.VITE_emailjs_publicKey
-        );
+  //  Send email using EmailJS in the browser
+  const handleEmailjs = async (values) => {
+    const emailParams = {
+      from_name: values.name,
+      from_email: values.email,
+      from_mobile: values.mobile,
+      message: values.message,
     };
 
-    const handleSubmit = async (values) => {
-        try {
-            setfromLoad(true);
-
-            //save data to fire store
-            await addDoc(userCollectionRef, values);
-
-            //emailjs
-            handleEmailjs(values);
-
-            //reset form and show/hide animation
-            form.reset();
-            setShowSubmitAnimation(true);
-            setTimeout(() => {
-                setShowSubmitAnimation(false);
-            }, 3000);
-            setfromLoad(false);
-            // console.log("Form submitted successfully!");
-        } catch (error) {
-            console.error("Error submitting form:", error);
-        }
-    };
-
-    return (
-        <div>
-            <form
-                onSubmit={form.onSubmit((values) => handleSubmit(values))}
-                className="space-y-4"
-            >
-                <TextInput
-                    withAsterisk
-                    label="Name"
-                    placeholder="Write Your Full name"
-                    // styles={{
-                    //   input: { background: "rgb(209 213 219)" },
-                    // }}
-                    {...form.getInputProps("name")}
-                />
-                <TextInput
-                    variant="filled"
-                    withAsterisk
-                    label="Email"
-                    placeholder="Write your Email"
-                    {...form.getInputProps("email")}
-                />
-
-                <div>
-                    <label className="text-sm font-medium">
-                        Mobile <span className="text-red-500">*</span>
-                    </label>
-                    <PhoneInput
-                        country={"in"} // Set the default country
-                        inputProps={{
-                            name: "mobile",
-                            autoFocus: true,
-                            autoComplete: "tel",
-                            placeholder: "Enter mobile number",
-                        }}
-                        className="w-full  text-gray-800 rounded-lg"
-                        inputStyle={{ width: "100%" }}
-                        value={form.values.mobile}
-                        onChange={(value) => form.setFieldValue("mobile", value)}
-                    />
-                    {form.errors.mobile && (
-                        <div className="text-red-500">{form.errors.mobile}</div>
-                    )}
-                </div>
-
-                <Textarea
-                    label="Message"
-                    placeholder="Write message"
-                    {...form.getInputProps("message")}
-                />
-
-                <Button type="submit" fullWidth>
-                    Submit
-                </Button>
-            </form>
-            {showSubmitAnimation && (
-                <div className="absolute top-0 flex items-center w-full justify-center  h-full ">
-                    <Lottie animationData={submitAnimation} id="submit" />
-                </div>
-            )}
-            {formLoad && (
-                <div className="absolute top-0 flex items-center w-full justify-center bg-skillBgHover  h-full ">
-                    <Lottie animationData={loadingCircleAnimation} id="submit" />
-                </div>
-            )}
-        </div>
+    await emailjs.send(
+      env.VITE_emailjs_serviceId,
+      env.VITE_emailjs_template_Id,
+      emailParams,
+      env.VITE_emailjs_publicKey
     );
+  };
+
+  const handleSubmit = async (values) => {
+    try {
+      setfromLoad(true);
+
+      const valuesToInsertInDB = {
+        name: values.name,
+        email: values.email,
+        mobile: values.mobile,
+        message: values.message,
+        createdAt: new Date().toLocaleString(),
+        updatedAt: new Date().toLocaleString(),
+      };
+
+      //save data to fire store
+      await addDoc(userCollectionRef, valuesToInsertInDB);
+
+      //emailjs
+    //   handleEmailjs(values);
+
+      //reset form and show/hide animation
+      form.reset();
+      setShowSubmitAnimation(true);
+      setTimeout(() => {
+        setShowSubmitAnimation(false);
+      }, 3000);
+      setfromLoad(false);
+      // console.log("Form submitted successfully!");
+    } catch (error) {
+      console.error("Error submitting form:", error);
+    }
+  };
+
+  return (
+    <div>
+      <form
+        onSubmit={form.onSubmit((values) => handleSubmit(values))}
+        className="space-y-4"
+      >
+        <TextInput
+          withAsterisk
+          label="Name"
+          placeholder="Write Your Full name"
+          // styles={{
+          //   input: { background: "rgb(209 213 219)" },
+          // }}
+          {...form.getInputProps("name")}
+        />
+        <TextInput
+          variant="filled"
+          withAsterisk
+          label="Email"
+          placeholder="Write your Email"
+          {...form.getInputProps("email")}
+        />
+
+        <div>
+          <label className="text-sm font-medium">
+            Mobile <span className="text-red-500">*</span>
+          </label>
+          <PhoneInput
+            country={"in"} // Set the default country
+            inputProps={{
+              name: "mobile",
+              autoFocus: true,
+              autoComplete: "tel",
+              placeholder: "Enter mobile number",
+            }}
+            className="w-full  text-gray-800 rounded-lg"
+            inputStyle={{ width: "100%" }}
+            value={form.values.mobile}
+            onChange={(value) => form.setFieldValue("mobile", value)}
+          />
+          {form.errors.mobile && (
+            <div className="text-red-500">{form.errors.mobile}</div>
+          )}
+        </div>
+
+        <Textarea
+          label="Message"
+          placeholder="Write message"
+          {...form.getInputProps("message")}
+        />
+
+        <Button type="submit" fullWidth>
+          Submit
+        </Button>
+      </form>
+      {showSubmitAnimation && (
+        <div className="absolute top-0 flex items-center w-full justify-center  h-full ">
+          <Lottie animationData={submitAnimation} id="submit" />
+        </div>
+      )}
+      {formLoad && (
+        <div className="absolute top-0 flex items-center w-full justify-center bg-skillBgHover  h-full ">
+          <Lottie animationData={loadingCircleAnimation} id="submit" />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default ContactForm;
